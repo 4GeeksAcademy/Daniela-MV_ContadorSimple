a9a9b32ac2e20c81c7290b984bd0576f56d04244
Daniela-MV <danmv.0991@gmail.com>
2025-04-19 18:38:27 +0000

    Se terminó de dar estilo al cronómetro

--- src/js/components/Cronometro.jsx
+++ src/js/components/Cronometro.jsx
@@ -1,33 +1,26 @@
 import React from "react";
 
-let Cronometro = (props)=> {
+let Cronometro = (props) => {
     return (
-    <div className="container text-center">
-        <div className="row bg-dark" style={{ width: "70rem", height: "10rem"}}>
-            <div className="col fs-1 text-warning">
-                <i className="bi bi-stopwatch"></i>
+        <div className="container text-center">
+            <div className="row bg-dark" style={{ width: "70rem", height: "10rem" }}>
+                <div className="col text-warning" style={{ fontSize: "90px" }}><i className="bi bi-stopwatch"></i></div>
+                <div className="col text-warning" style={{ fontSize: "80px" }} >{props.digSeis}</div>
+                <div className="col text-warning" style={{ fontSize: "80px" }}>{props.digCinco}</div>
+                <div className="col text-warning" style={{ fontSize: "80px" }}>{props.digCuatro}</div>
+                <div className="col text-warning" style={{ fontSize: "80px" }}>{props.digTres}</div>
+                <div className="col text-warning" style={{ fontSize: "80px" }}>{props.digDos}</div>
+                <div className="col text-warning" style={{ fontSize: "80px" }}>{props.digUno}</div>
             </div>
-            <div className="col fs-1 text-warning">
-                {props.digSeis}
-            </div>
-            <div className="col fs-1 text-warning">
-                {props.digCinco}
-            </div>
-            <div className="col fs-1 text-warning">
-                {props.digCuatro}
-            </div>
-            <div className="col fs-1 text-warning">
-                {props.digTres}
-            </div>
-            <div className="col fs-1 text-warning">
-                {props.digDos}
-            </div>
-            <div className="col fs-1 text-warning">
-                {props.digUno}
-            </div>
+
+            {/* <div className="input-group mb-3">
+                <button className="btn btn-outline-secondary" type="button" id="button-addon1">Button</button>
+                <input type="text" className="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">
+            </div> */}
+
+
         </div>
-    </div>
     )
-}
+};
 
 export default Cronometro;
