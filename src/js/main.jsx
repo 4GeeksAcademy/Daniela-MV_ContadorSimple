530eb1492e3d9e5150b5025f3ab0af21b8e253db
Daniela-MV <danmv.0991@gmail.com>
2025-04-24 14:50:05 +0000

    ya tengo el contador con la alerta

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -14,36 +14,36 @@ import Home from './components/Home';
 
 
 let conteo = 0;
-let esReverso = false  
-function cuentaRegresiva(numero) {
-conteo = numero;
-esReverso = true;
-}
+
+const root =  ReactDOM.createRoot(document.getElementById('root'))
+
+const userInput = prompt("Colaca un número:")
+  setInterval(() => {
+    let primeraCasilla = Math.floor((conteo / 1) % 10);
+    let segundaCasilla = Math.floor((conteo / 10) % 10);
+    let terceraCasilla = Math.floor((conteo / 100) % 10);
+    let cuartaCasilla = Math.floor((conteo / 1000) % 10);
+    let quintaCasilla = Math.floor((conteo / 10000) % 10);
+    let sextaCasilla = Math.floor((conteo / 100000) % 10);
+
+   root.render(
+      <React.StrictMode>
+        <Home digUno={primeraCasilla} digDos={segundaCasilla} digTres={terceraCasilla} digCuatro={cuartaCasilla} digCinco={quintaCasilla} digSeis={sextaCasilla}/>
+      </React.StrictMode>
+    )
+    if (conteo === parseInt(userInput)) {
+      alert("Haz llegado a ${userInput}")
+    }
+    conteo ++;
+  }, 1000);
 
 
-let contador = setInterval(() => {
-let primeraCasilla = Math.floor((conteo/1) %10);
-let segundaCasilla = Math.floor((conteo/10) %10);
-let terceraCasilla = Math.floor((conteo/100) %10);
-let cuartaCasilla = Math.floor((conteo/1000) %10);
-let quintaCasilla = Math.floor((conteo/10000) %10);
-let sextaCasilla = Math.floor((conteo/100000) %10);
-
-if (esReverso === false){
-  conteo ++;
-} else{
-  conteo --;
-}
 
 
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
- <React.StrictMode>
-    <Home digUno = {primeraCasilla} digDos={segundaCasilla} digTres = {terceraCasilla} digCuatro = {cuartaCasilla} digCinco = {quintaCasilla} digSeis = {sextaCasilla} cuentaRegresiva ={cuentaRegresiva} />
-  </React.StrictMode>,
-)
-}, 1000);
+
+
 
 
 
